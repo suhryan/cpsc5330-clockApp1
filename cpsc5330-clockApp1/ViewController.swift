6318654ec35fed1382aa1be4e52faa0900217213
suhryan <ryansuh2002@gmail.com>
2023-10-31 11:40:55 -0700

    Initial Commit

---
+++ cpsc5330-clockApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cpsc5330-clockApp1
+//
+//  Created by ryan suh on 10/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
