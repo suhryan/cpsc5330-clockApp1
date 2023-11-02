29afffe7312da1044c758a7e1e939554bc3ad776
suhryan <ryansuh2002@gmail.com>
2023-11-01 17:29:39 -0700

    Live clock working.

--- cpsc5330-clockApp1/ViewController.swift
+++ cpsc5330-clockApp1/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer1: Timer?
+    var currentDate: Date {
+        return Date()
+    }
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ss"
+        return formatter
+    }()
+    @IBOutlet weak var liveClockLabel: UILabel!
+    
+    @IBOutlet weak var timePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        startClock()
+
     }
-
-
+    
+    func startClock() {
+        timer1 = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+            let currentHour = Calendar.current.component(.hour, from: self.currentDate)
+            if currentHour < 12 {
+                // Set AM background image
+            } else {
+                // Set PM background image
+            }
+            self.liveClockLabel.text = self.dateFormatter.string(from: self.currentDate)
+        }
+    }
 }
 
